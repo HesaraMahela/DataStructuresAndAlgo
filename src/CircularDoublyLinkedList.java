35466ab0fc96e71cbbcce319eaba3321db1129d0
HesaraMahela <hesaramahela@outlook.com>
2023-09-28 13:30:27 +0530

    feat:circular doublylneked insert with index was added not working properly

--- src/CircularDoublyLinkedList.java
+++ src/CircularDoublyLinkedList.java
@@ -2,7 +2,22 @@ public class CircularDoublyLinkedList {
  DoublyLinkedListNode Head;
  DoublyLinkedListNode Tail;
 
+ public static void main(String[] args){
+        CircularDoublyLinkedList list = new CircularDoublyLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+     System.out.println(list.Head.prev);
+        list.insert(8,5);
 
+     System.out.println(list.Head.prev);
+        //list.delete(5);
+       list.print();
+
+
+ }
  public void insert(int data){
      DoublyLinkedListNode node = new DoublyLinkedListNode(data);
      if (Head==null){ // inserting first element
@@ -14,8 +29,8 @@ public class CircularDoublyLinkedList {
          node.prev =Tail;
          Tail.next = node;
          Tail = node;
-
-         Tail.next = Head;
+         node.next = Head;
+         Head.prev = Tail;
      }
  }
 
@@ -35,28 +50,13 @@ public class CircularDoublyLinkedList {
      System.out.println(temp + "]");
  }
 
- public static void main(String args[]){
-     CircularDoublyLinkedList list = new CircularDoublyLinkedList();
-     list.insert(1);
-     list.insert(2);
-     list.insert(3);
-     list.insert(4);
-     list.insert(5);
-     list.delete(5);
-     list.print();
 
-
- }
 private void deleteNode(DoublyLinkedListNode temp){
     DoublyLinkedListNode prevNode = temp.prev;
     DoublyLinkedListNode nextNode = temp.next;
     prevNode.next = nextNode;
     nextNode.prev = prevNode;
 }
-
-
-
-
 
 public void delete(int val){
      /* loop through the list head to tail if the value was found delete it and set it */
@@ -85,6 +85,38 @@ public void delete(int val){
         deleteNode(temp);
         Tail = Tail.prev;
     }
+}
+
+
+public void insert(int data, int index){
+     // travel to that index consider the (ending or not circular)
+    DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+    DoublyLinkedListNode temp = Head;
+
+    for(int i=0; i< index;i++){// get the node the index
+        temp = temp.next;
+    }
+
+    DoublyLinkedListNode prevNode = temp.prev;
+    DoublyLinkedListNode nextNode = temp;
+    newNode.prev=prevNode;
+    prevNode.next =newNode;
+
+
+    newNode.next =nextNode;
+    nextNode.prev =newNode;
+
+    System.out.println(newNode.prev +" - " +newNode.next);
+
+    System.out.println(newNode.prev);
+    if(index==0){ // inserting to the first element // todo : dose not work with newNode.perv == tail
+        Head =newNode;
+    }
+
+    if(newNode.prev == Tail){
+        Tail = newNode;
+    }
+
 }
 
 }
