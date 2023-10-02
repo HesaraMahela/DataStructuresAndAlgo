08e2dd666522535018bdbb4cd2f4fdd7d6a37785
HesaraMahela <hesaramahela@outlook.com>
2023-10-02 14:48:20 +0530

    right insertion sort method

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class insertionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{3,2,9,1,5,6,4,11};
-        inSort(arr);
+        insertionsort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -15,9 +15,23 @@ public class insertionSort {
             while (j>0 && arr[j-1]>temp){
                 // swap them temp with j-1
                 arr[j] = arr[j-1];
-                arr[j-1]=temp;
                 j--;
+                //arr[j-1]=temp;
+                // j--; this is less efficient
             }
+            arr[j] = temp;
+
+        }
+    }
+    public static void insertionsort(int[] arr){
+        for(int j=1; j<arr.length; j++){ // 0 th element is already sorted
+           int key = arr[j];
+           int i =j-1;
+           while(i>=0 && arr[i]>key){
+               arr[i+1]=arr[i];
+               i--;
+           }
+           arr[i+1]=key;
         }
     }
 }
